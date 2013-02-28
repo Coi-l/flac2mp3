540df4286811e2c2430065810189391d2e5060d1
Coil <dev@skyshaper.net>
2013-02-28 22:13:00 +0100

    tags moved, transcoding ok

--- flac2mp3.py
+++ flac2mp3.py
@@ -3,6 +3,12 @@ import sys
 import subprocess
 import Queue
 import threading
+import mutagen
+
+from mutagen.flac import FLAC
+from mutagen.easyid3 import EasyID3
+from mutagen.mp3 import MP3
+from mutagen.id3 import TIT2
 
 flac_files = []
 mp3_files = []
@@ -10,10 +16,10 @@ mp3_files = []
 flac_dir = 'flac'
 mp3_dir = 'mp3'
 
-file_queue = Queue.Queue()
+transcode_queue = Queue.Queue()
 
 def is_flac_file(file):
-    return True
+    return file.endswith(".flac")
 
 def read_dir(dir):
     files = os.listdir(dir)
@@ -44,7 +50,7 @@ def prepare_transcode_queue(out_dir):
         dir = os.path.split(mp3f)[0]
         if not os.path.exists(dir):
             os.makedirs(dir)
-        file_queue.put(f)
+        transcode_queue.put(f)
 
 class ThreadTranscode(threading.Thread):
     def __init__(self, queue):
@@ -53,18 +59,45 @@ class ThreadTranscode(threading.Thread):
 
     def run(self):
         while not self.queue.empty():
+            file_pair = None
             try:
                 file_pair = self.queue.get_nowait()
-                print "Transcoding file %s into %s" % file_pair
-                flac = ['flac', '-d', '-c', file_pair[0]]
-                lame = ['lame', '-V0', '-', file_pair[1]]
-                p_flac = subprocess.Popen(flac, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                p_lame = subprocess.Popen(lame, stdin=p_flac.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                p_lame.wait()
-                (o, e) = p_lame.communicate()
-                self.queue.task_done()
-            except:
+            except Exception as e:
+                print e
                 print "Nothing more in the work Queue"
+
+            if file_pair:
+                try:
+                    print "Transcoding file %s into %s" % file_pair
+                    flac = ['flac', '-d', '-c', file_pair[0]]
+                    lame = ['lame', '-V0', '-', file_pair[1]]
+                    p_flac = subprocess.Popen(flac, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    p_lame = subprocess.Popen(lame, stdin=p_flac.stdout, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                    p_lame.wait()
+                    (o, e) = p_lame.communicate()
+                    self.move_tags(file_pair)
+                except Exception as e:
+                    print e
+
+                self.queue.task_done()
+
+    def move_tags(self, file_pair):
+        print "Moving tags from %s to %s" % file_pair
+        self.write_default_tag(file_pair[1])
+        flac = FLAC(file_pair[0])
+        mp3 = EasyID3(file_pair[1])
+        for key in flac:
+            try:
+                mp3[key] = flac[key]
+            except Exception as e:
+                pass
+                #print e
+        mp3.save()
+
+    def write_default_tag(self, mp3_file_name):
+        mp3 = MP3(mp3_file_name)
+        mp3["TIT2"] = TIT2(encoding=3, text=["Default"])
+        mp3.save()
 
 read_flac_dir(flac_dir)
 prepare_files_list(flac_dir, mp3_dir)
@@ -73,11 +106,11 @@ prepare_transcode_queue(mp3_dir)
 print "Transcoding started"
 
 for i in range(4):
-    t = ThreadTranscode(file_queue)
+    t = ThreadTranscode(transcode_queue)
     t.daemon = True
     t.start()
 
-file_queue.join()
+transcode_queue.join()
 
 print "Transcoding complete"
 
